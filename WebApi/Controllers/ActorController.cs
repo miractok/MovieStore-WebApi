ee18fa5a572fb24dd8e269ea73f1ee231b934b88
Muminarous <miractok4@gmail.com>
2023-03-20 14:02:57 +0300

    Film(not completed) + Actor Controller

---
+++ WebApi/Controllers/ActorController.cs
@@ -0,0 +1,86 @@
+using AutoMapper;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Application.ActorOperations.Commands.CreateActor;
+using WebApi.Application.ActorOperations.Commands.DeleteActor;
+using WebApi.Application.ActorOperations.Commands.UpdateActor;
+using WebApi.Application.ActorOperations.Queries.GetActor;
+using WebApi.Application.ActorOperations.Queries.GetActorDetails;
+using WebApi.DBOperations;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[Controller]s")]
+public class ActorController : ControllerBase
+{
+    private readonly IMovieStoreDbContext _context;
+    private readonly IMapper _mapper;
+
+    public ActorController(IMapper mapper, IMovieStoreDbContext context)
+    {
+        _mapper = mapper;
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetMovies()
+    {
+        GetActorsQuery query = new GetActorsQuery(_mapper,_context);
+        var result = query.Handle();
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    [return: System.Diagnostics.CodeAnalysis.MaybeNull]
+    public IActionResult GetById(int id)
+    {
+        ActorsViewIdModel result;
+        GetActorDetailQuery query = new GetActorDetailQuery(_context, _mapper);
+        query.ActorId = id;
+        GetActorDetailQueryValidator validator = new GetActorDetailQueryValidator();
+        validator.ValidateAndThrow(query);
+        result = query.Handle();
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public IActionResult AddActor([FromBody] CreateActorViewModel newActor)
+    {
+        CreateActorCommand command = new CreateActorCommand(_mapper,_context);
+        command.Model = newActor;
+        CreateActorCommandValidator validator = new CreateActorCommandValidator();
+        validator.ValidateAndThrow(command);
+
+        command.Handle();
+        return Ok();
+    }
+
+    [HttpDelete("id")]
+    public IActionResult DeleteActor(int id)
+    {
+        DeleteActorCommand command = new DeleteActorCommand(_context);
+        command.ActorId = id;
+
+        DeleteActorCommandValidator validator = new DeleteActorCommandValidator();
+        validator.ValidateAndThrow(command);
+
+        command.Handle();
+        return Ok();
+    }
+
+    [HttpPut("id")]
+    public IActionResult UpdateActor(int id,[FromBody] UpdateActorModel updateActor)
+    {
+        UpdateActorCommand command = new UpdateActorCommand(_context);
+        command.ActorId = id;
+        command.Model = updateActor;
+        
+        UpdateActorCommandValidator validator = new UpdateActorCommandValidator();
+        validator.ValidateAndThrow(command);
+
+        command.Handle();
+        return Ok();
+    }
+}
--- WebApi/Controllers/FilmController.cs
+++ WebApi/Controllers/FilmController.cs
@@ -1,10 +1,44 @@
+using AutoMapper;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Application.FilmOperations.Queries.GetMovies;
+using WebApi.Application.FilmOperations.Queries.GetMovieDetails;
+using WebApi.DBOperations;
 
 namespace WebApi.Controllers;
 
 [ApiController]
-[Route("[controller]s")]
+[Route("[Controller]s")]
 public class FilmController : ControllerBase
 {
-    
+    private readonly IMovieStoreDbContext _context;
+    private readonly IMapper _mapper;
+
+    public FilmController(IMapper mapper, IMovieStoreDbContext context)
+    {
+        _mapper = mapper;
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetMovies()
+    {
+        GetMoviesQuery query = new GetMoviesQuery(_context,_mapper);
+        var result = query.Handle();
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    [return: System.Diagnostics.CodeAnalysis.MaybeNull]
+    public IActionResult GetById(int id)
+    {
+        FilmsViewIdModel result;
+        GetMovieDetailQuery query = new GetMovieDetailQuery(_context, _mapper);
+        query.FilmId = id;
+        GetMovieDetailQueryValidator validator = new GetMovieDetailQueryValidator();
+        validator.ValidateAndThrow(query);
+        result = query.Handle();
+
+        return Ok(result);
+    }
 }
